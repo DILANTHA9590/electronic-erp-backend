e5877120750647fd07ca2d74c4fd2c892b340e3a
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-02-09 23:25:31 +0530

    setup TypeORM and environment config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './api/auth/auth.module';
 import { UserModule } from './api/user/user.module';
-import { ConfigModule } from '@nestjs/config'; 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 
 
 @Module({
   imports: [
-    
     ConfigModule.forRoot({
-      isGlobal: true,   
+      isGlobal: true,
+      envFilePath: '.env',
     }),
-    
-    AuthModule, UserModule],
+
+    ScheduleModule.forRoot(),
+     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+        migrations: ['dist/migrations/*.js'],
+        logging: ['query', 'error'],//dev
+    // logging: ['error'],//prodction
+        retryAttempts: 5, 
+        retryDelay: 3000,
+      }),
+    }),
+
+    UserModule,
+    AuthModule,
+  ]
 
 })
 export class AppModule {}
