16942a397a3f4c09020e46b5b4756a618155b534
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-02-14 23:31:37 +0530

    Install argon2 for password hashing

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,12 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getRandomValues } from 'crypto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    constructor(
+      @InjectRepository
+      (User) private readonly userRepository: Repository<User>,
+      private configService: ConfigService
+    ){}
+
+async create(createUserDto: CreateUserDto) {
+  const { email, user_name, password } = createUserDto;
+
+  // 🔎 Check existing user (single DB query)
+  const existingUser = await this.userRepository.findOne({
+    where: [
+      { email },
+      { user_name }
+    ],
+  });
+
+  if (existingUser) {
+    if (existingUser.email === email) {
+      throw new ConflictException('Email already taken');
+    }
+    if (existingUser.user_name === user_name) {
+      throw new ConflictException('Username already taken');
+    }
   }
+
+  // 🔐 Get Pepper from .env
+  const pepper = this.configService.getOrThrow<string>('PASSWORD_PEPPER');
+
+  // 🔒 Hash password using Argon2id
+  const hashedPassword = await argon2.hash(password + pepper, {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16, // 64MB
+    timeCost: 3,
+    parallelism: 1,
+  });
+
+  // 👤 Create user
+  const newUser = this.userRepository.create({
+    ...createUserDto,
+    password: hashedPassword,
+    isVerified: false, // OTP verify karanawanam false
+  });
+
+  await this.userRepository.save(newUser);
+
+  return {
+    success: true,
+    message: 'User created successfully',
+  };
+}
+
+
+
+
+  private generateFiveDigitNumber(): number {
+  return Math.floor(100 + Math.random() * 900);
+}
+
 
   findAll() {
     return `This action returns all user`;
