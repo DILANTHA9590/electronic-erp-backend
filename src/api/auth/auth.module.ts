a16bd768ff98e6b537826788e9fa479a0786db82
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-02-20 18:46:44 +0530

    configure JwtModule with async register and environment secret

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
