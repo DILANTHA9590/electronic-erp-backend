79c085aab956594a6ddbe5eab080a39c3bf3eac5
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-02-20 18:46:18 +0530

    replace default CRUD with login endpoint and JWT auth flow

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthDto } from './dto/login-dto';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Post('login')
+  @HttpCode(HttpStatus.OK) // 200
+  create(@Body() createAuthDto:AuthDto) {
+    return this.authService.loginUser(createAuthDto);
   }
 
   @Get()
@@ -22,10 +23,6 @@ export class AuthController {
     return this.authService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
