a2d9e11d499e72f01a1cd03943f73391b6bea35e
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-05-07 11:37:37 +0530

    change token interface fields to camelCase ✨

--- src/api/auth/interfaces/auth.interface.ts
+++ src/api/auth/interfaces/auth.interface.ts
@@ -2,26 +2,26 @@ import { USER_STATUS } from "src/api/user/entities/user-status.enum";
 
 export interface accessToken {
     sub:string,
-    first_name:string,
-    last_name?:string,
-    token_version?:number,
+    firstName:string,
+    lastName?:string,
+    tokenVersion?:number,
     email?:string
-    user_status:USER_STATUS
+    userStatus:USER_STATUS
 
 }
 
 
 export interface refreshToken  {
     sub:string,
-    token_version?:number,
+    tokenVersion?:number,
 }
 // auth/interfaces/token-payload.interface.ts
 
 export interface TokenPayload {
   id: string;
-  first_name: string;
-  last_name?: string;
+  firstName: string;
+  lastName?: string;
   email: string;
-  token_version: number;
-  user_status: USER_STATUS;
+  tokenVersion: number;
+  userStatus: USER_STATUS;
 }
